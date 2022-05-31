d834ccd89a8838669a127328c9d0e3a1caf04eec
Kerly Sousa <kerlygee@gmail.com>
2022-05-31 00:51:39 -0300

    Requisito 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-class FeedBack extends Component {
+class Feedback extends Component {
+  redirectButton = () => {
+    const { history } = this.props;
+    history.push('/ranking');
+  }
+
   render() {
+    const { redirectButton } = this;
     return (
       <div>
         <Header />
         <p data-testid="feedback-text">Feedback</p>
+        <button
+          type="button"
+          onClick={ redirectButton }
+          data-testid="btn-ranking"
+        >
+          Ranking
+
+        </button>
       </div>
     );
   }
 }
 
-export default FeedBack;
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Feedback;
