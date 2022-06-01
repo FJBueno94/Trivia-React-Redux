6dd0bc5c88075b688999fb53748b5dee75c3182a
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-01 16:34:13 -0300

    feat: adicionando css da pag login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { FaPlay } from 'react-icons/fa';
-import { AiFillSetting } from 'react-icons/ai';
 import { addPlayerAction, getTokenThunk, addScoreAction } from '../redux/actions';
 import logo from '../trivia.png';
 
@@ -65,57 +63,44 @@ render() {
       disabled,
     }, settingButton, onclick, handleChange } = this;
   return (
-    <div className="flex flex-col items-center">
-      <header className="mt-10">
+    <div>
+      <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
       </header>
-      <form
-        className="flex flex-col justify-around rounded w-80 h-72 mt-[15%] p-8 bg-white"
-      >
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={ name }
-          onChange={ handleChange }
-          data-testid="input-player-name"
-          className="border-b-2 border-purple-700 text-center p-2 outline-none
-          focus:border-blue-500 transition-all ease-in duration-300"
-          placeholder="Nome"
-        />
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={ email }
-          data-testid="input-gravatar-email"
-          onChange={ handleChange }
-          className="border-b-2 border-purple-700 text-center p-2 outline-none
-          focus:border-blue-500 transition-all ease-in duration-300"
-          placeholder="Email"
-        />
-        <div className="flex justify-around">
-          <button
-            type="submit"
-            onClick={ onclick }
-            data-testid="btn-play"
-            disabled={ disabled }
-            className="flex items-center justify-center
-            bg-purple-700 rounded p-3 text-white w-2/5
-            disabled:text-gray disabled:bg-purple-300 hover:bg-purple-900"
-          >
-            <FaPlay />
-          </button>
-          <button
-            data-testid="btn-settings"
-            type="button"
-            onClick={ settingButton }
-            className="flex items-center justify-center
-            bg-blue-500 rounded p-3 text-white w-2/5 hover:bg-blue-700"
-          >
-            <AiFillSetting />
-          </button>
-        </div>
+      <form>
+        <label htmlFor="name">
+          Nome:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={ name }
+            onChange={ handleChange }
+            data-testid="input-player-name"
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={ email }
+            data-testid="input-gravatar-email"
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={ onclick }
+          data-testid="btn-play"
+          disabled={ disabled }
+        >
+          Play
+        </button>
+        <button data-testid="btn-settings" type="button" onClick={ settingButton }>
+          Settings
+        </button>
       </form>
     </div>
   );
